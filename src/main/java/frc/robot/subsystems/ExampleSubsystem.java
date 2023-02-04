eaf935eeeaea28e1e733ef494174293da5019bad
Nitya Bhartiya <nitya@Nityas-MacBook-Air-2.local>
2023-02-04 14:55:25 -0800

    new changes2 :)

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,35 +5,30 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.util.MotorTest;
 import frc.robot.util.MotorTestInternalSpark;
 
-import java.util.List;
-
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import frc.robot.util.MotorTest;
-
-import SushiFrcLib.Motor.MotorHelper;
 
 public class ExampleSubsystem extends SubsystemBase {
   //private TalonFX driveMotor;
   //WPI_TalonFX motor = new WPI_TalonFX(11);
-  CANSparkMax mod1Drive;
-  CANSparkMax mod2Drive;
-  CANSparkMax mod3Drive;
-  CANSparkMax mod4Drive;
+  public CANSparkMax mod1Drive;
+  public CANSparkMax mod2Drive;
+  public CANSparkMax mod3Drive;
+  public CANSparkMax mod4Drive;
+
+
   MotorTestInternalSpark motorInternalSpark1;
   MotorTestInternalSpark motorInternalSpark2;
   MotorTestInternalSpark motorInternalSpark3;
   MotorTestInternalSpark motorInternalSpark4;
+
   MotorTest motorTest;
   ExampleSubsystem instance = null;
 
@@ -45,7 +40,7 @@ public class ExampleSubsystem extends SubsystemBase {
   }
 
   /** Creates a new ExampleSubsystem. */
-  private ExampleSubsystem(){
+  public ExampleSubsystem(){
     mod1Drive = new CANSparkMax(1, CANSparkMax.MotorType.kBrushless);
     mod2Drive = new CANSparkMax(4, CANSparkMax.MotorType.kBrushless);
     mod3Drive = new CANSparkMax(7, CANSparkMax.MotorType.kBrushless);
@@ -84,10 +79,6 @@ public class ExampleSubsystem extends SubsystemBase {
     }
   }
 
-  // public void flipMotor(Boolean flipped){
-  //   motor.set(speed*-1);
-  // }
-
   public void flipEncoder(WPI_TalonFX motor, Boolean flipped){
     motor.setInverted(flipped);
 
@@ -109,33 +100,6 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Motor Speed", mod1Drive.get());
-  }
-
-  public void runMotor(CANSparkMax motor, List<String> inputs){
-      MotorTest.setSpeed(motor, Double.parseDouble(inputs.get(0)));
-      MotorTest.coastOrBrake(motor, Integer.parseInt(inputs.get(1)));
-      MotorTest.invertMotor(motor,inputs.get(2).equals("1"));
-      MotorTest.setCurrentLimit(motor, Integer.parseInt(inputs.get(3)) );
-      MotorTest.setEncoderLimit(motor, Integer.parseInt(inputs.get(4)), Integer.parseInt(inputs.get(5)));
-  }
-
-  public void newTestInput(List<String> inputs) {
-    CANSparkMax testMotor;
-
-    System.out.println(inputs);
-
-    switch (inputs.get(0)) {
-      case "mod1Drive":
-        testMotor = mod1Drive;
-        break;
-      default:
-        testMotor = null;
-        break;
-    }
-
-    if (testMotor != null) {
-      runMotor(testMotor, inputs.subList(1, inputs.size()));
-    }
   }
 
   @Override
--- src/main/java/frc/robot/util/MotorTest.java
+++ src/main/java/frc/robot/util/MotorTest.java
@@ -2,20 +2,10 @@ package frc.robot.util;
 
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.MotorTest;
 import java.util.List;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
-
-import SushiFrcLib.Motor.MotorHelper;
 
 
 public class MotorTest {
@@ -30,61 +20,93 @@ public class MotorTest {
     return instance;
   }
 
-    private MotorTest(){
+    public MotorTest(){
 
     }
 
-    // neos
-    public static boolean coastOrBrake(CANSparkMax motor, int num){
-        if(num==0){
-          return (motor.getIdleMode()==IdleMode.kCoast);
-          //motor.setIdleMode(IdleMode.kCoast);
+    public void isUpdated(){
+      if (SmartDashboard.getString("changed?", null) == "true"){
+        for (int i = 0; i < motorList.size(); i++){
+          coastOrBrake(motorList.get(i).motor);
+          invertMotor(motorList.get(i).motor);
+          setCurrentLimit(motorList.get(i).motor);
+          setEncoderLimit(motorList.get(i).motor);
         }
-        else{
-          return (motor.getIdleMode()==IdleMode.kBrake);
-          //motor.setIdleMode(IdleMode.kBrake);
-        }
+      }
     }
 
-    public void runMotor(CANSparkMax motor, List<String> inputs){
-      //speed = Double.parseDouble(inputs.get(0));
-      MotorTest.coastOrBrake(motor, Integer.parseInt(inputs.get(1)));
+    public void isRun(){
+      if (SmartDashboard.getString("running?", null) == "true"){
+        for (int i = 0; i < motorList.size(); i++){
+          setSpeed(motorList.get(i).motor);
+        }
+      }
+    }
 
-      MotorTest.setCurrentLimit(motor, Integer.parseInt(inputs.get(3)) );
-      MotorTest.setEncoderLimit(motor, Integer.parseInt(inputs.get(4)), Integer.parseInt(inputs.get(5)));
+    public void isStop(){
+      if (SmartDashboard.getString("running", null) == "false"){
+        for (int i = 0; i < motorList.size(); i++){
+          stopMotors(motorList.get(i).motor);
+        }
+      }
+    }
+
+    public static void coastOrBrake(CANSparkMax motor){
+      String[] array = SmartDashboard.getStringArray("dataTable", null);
+      for (int i = 0; i < array.length; i++){
+        String[] motorArray = (array[i]).split(" ");
+        IdleMode isCoast;
+        if ((motorArray[6]) == "false"){
+          isCoast = IdleMode.kBrake;
+        } else {
+          isCoast = IdleMode.kCoast;
+        }
+        motorList.get(i).setIdleMode(motor, isCoast);
+      }
     }
     
 
-    public static boolean invertMotor(CANSparkMax motor, int motorNum){
-        String[] array = SmartDashboard.getStringArray("dataTable", null);
-        String[] motorArray = (array[motorNum]).split(" ");
-
-        boolean flipped = (motorArray[7].equals("1"));
-        motor.setInverted(flipped);
-        return(motor.getInverted()==flipped);
+    public static void invertMotor(CANSparkMax motor){
+      String[] array = SmartDashboard.getStringArray("dataTable", null);
+      for (int i = 0; i < array.length; i++){
+        String[] motorArray = (array[i]).split(" ");
+        boolean isInverted = motorArray[7] == "false";
+        motorList.get(i).invertMotor(motor, isInverted);
+      }
     }
 
-
-    //SETSPEED(MOTOR1)
     public static void setSpeed(CANSparkMax motor){
         String[] array = SmartDashboard.getStringArray("dataTable", null);
         for (int i = 0; i < array.length; i++){
           String[] motorArray = (array[i]).split(" ");
-          double speed = (Double.parseDouble(motorArray[4]));
-          motorList.get(i).setSpeed(motor, speed);
+          double constSpeed = (Double.parseDouble(motorArray[4]));
+          double joystickSpeed = (Double.parseDouble(motorArray[5]));
+          double joystickVal = 1;
+          motorList.get(i).setSpeed(motor, constSpeed + joystickVal * joystickSpeed);
         }
     }
     
-    public static boolean setCurrentLimit(CANSparkMax motor, int currentLimit){
-        return (motor.getOutputCurrent() < currentLimit);
+    public static void stopMotors(CANSparkMax motor){
+      motor.disable();
     }
+
+    public static void setCurrentLimit(CANSparkMax motor){
+      String[] array = SmartDashboard.getStringArray("dataTable", null);
+      for (int i = 0; i < array.length; i++){
+        String[] motorArray = (array[i]).split(" ");
+        int currLimit = (Integer.parseInt(motorArray[8]));
+        motorList.get(i).setCurrentLimit(motor, currLimit);
+      }
+  }
     
-    public static boolean setEncoderLimit(CANSparkMax motor, double lowLimit, double highLimit){
-        if (motor.getEncoder().getPosition() <lowLimit || motor.getEncoder().getPosition() >highLimit){
-          //motor.disable();
-          return false;
-        }
-        return true;
+    public static void setEncoderLimit(CANSparkMax motor){
+      String[] array = SmartDashboard.getStringArray("dataTable", null);
+      for (int i = 0; i < array.length; i++){
+        String[] motorArray = (array[i]).split(" ");
+        double lowLimit = (Double.parseDouble(motorArray[9]));
+        double highLimit = (Double.parseDouble(motorArray[10]));     
+        motorList.get(i).setEncoderLimit(motor, lowLimit, highLimit);
+      }
     }
 
     public void registerMotor(MotorTestInternalSpark motorInternalSpark, String string, String string2,
